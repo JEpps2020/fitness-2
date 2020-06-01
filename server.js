35d6537280e152149beb03de762115548fd10af7
Jasmine Epps <epps.jasmine@gmail.com>
2020-05-31 22:10:17 -0400

    heroku server.js deployment

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user2:password2@ds143678.mlab.com:43678/heroku_gd3pgg9f";
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useFindAndModify: false }); 
+mongoose.connect(MONGODB_URI, { useMongoClient: true, useNewUrlParser: true, useFindAndModify: false }); 
 
 require("./routes/api-routes")(app);
 require("./routes/html-routes")(app);
